7c5599933090b6615729dbc51c9939e3f722537e
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-11-07 11:01:00 -0800

    Build cookie parser middleware

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,13 +1,16 @@
 const parseCookies = (req, res, next) => {
   var cookie = req.headers.cookie;
+  var obj = {};
   if (cookie !== undefined) {
     var cookieSplit = cookie.split(';');
-    for (var i = 0; i < cookieSplit; i++) {
-      if (cookieSplit[i] === '=') {
-        cookieSplit[i] = ':';
-      }
-    } 
+    for (var i = 0; i < cookieSplit.length; i++) {
+      var eachCookieArr = cookieSplit[i].split('='); 
+      obj[eachCookieArr[0].trim()] = eachCookieArr[1].trim(); 
+    }
   }
+
+  req.cookies = obj;
+  next();
 };
 
 module.exports = parseCookies;
